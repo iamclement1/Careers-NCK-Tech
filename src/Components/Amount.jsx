cd3288b1b34cdab467e28c4203d5601cfe6b5b1a
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-03 03:34:00 +0100

    added total fee

--- src/Components/Amount.jsx
+++ src/Components/Amount.jsx
@@ -19,7 +19,29 @@ const Amount = () => {
                     <h2>Estimated TAX</h2>
                 </div>
                 <div className='font-medium'>
-                    <p>&#8358;2,497.00</p>
+                    <p>&#8358;119.64</p>
+                </div>
+            </div>
+            <div className='flex justify-between pt-4'>
+                <div className='font-semibold'>
+                    <h2>Promotional Code:<span className='text-gray-500'>Z4KXLM9A</span></h2>
+                    
+                </div>
+                <div className='font-medium'>
+                    <p>&#8358;119.64</p>
+                </div>
+            </div>
+            <div className='pt-10'>
+                <hr />
+            </div>
+            <div className='flex justify-between pt-4'>
+                <div className='font-semibold'>
+                    <button class="bg-blue-500 hover:bg-blue-500 text-white font-bold py-4 px-14 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Complete Payment
+                    </button>
+                </div>
+                <div className='font-extrabold text-2xl'>
+                    <p>Total &#8358;2556.64</p>
                 </div>
             </div>
         </div>
